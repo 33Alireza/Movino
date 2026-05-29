8451c3f863eecc31fba6c9e840490adbdbcb0df3
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-05-28 19:32:16 -0800

    Implement body or throw (#18)

--- app/src/main/java/com/example/movino/api/Client.kt
+++ app/src/main/java/com/example/movino/api/Client.kt
@@ -1,14 +1,18 @@
 package com.example.movino.api
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.ANDROID
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.URLProtocol
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
+import io.ktor.http.isSuccess
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -27,7 +31,13 @@ fun initialization() = HttpClient {
         json(
             Json {
                 ignoreUnknownKeys = true
-            }
-        )
+            })
     }
+}
+
+suspend inline fun <reified T> bodyOrThrow(response: HttpResponse): T {
+    if (!response.status.isSuccess()) {
+        throw Exception(response.bodyAsText())
+    }
+    return response.body<T>()
 }
