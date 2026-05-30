f09eac09d46feab524c758c9e360a7fdf7a3fbd7
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-05-29 21:27:10 -0800

    Implement network module (#22)

---
+++ app/src/main/java/com/example/movino/di/NetworkModule.kt
@@ -0,0 +1,17 @@
+package com.example.movino.di
+
+import com.example.movino.api.initialization
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideHttpClient(): HttpClient = initialization()
+}
