c9d3faa93a9e2306b055a2e852c31441803b38c1
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-05-31 19:24:05 -0800

    Implement splash screen (#30)

--- app/src/main/java/com/example/movino/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/movino/navigation/AppNavigation.kt
@@ -4,19 +4,41 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-
+import com.example.movino.view.DetailScreen
+import com.example.movino.view.HomeScreen
+import com.example.movino.view.SearchScreen
+import com.example.movino.view.SplashScreen
 
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
 
     NavHost(
-        navController = navController,
-        startDestination = Splash
+        navController = navController, startDestination = Splash
     ) {
-        composable<Splash> {}
-        composable<Home> {}
-        composable<Search> {}
-        composable<Detail> {}
+        composable<Splash> {
+            SplashScreen(
+                navigateToHomeScreen = {
+                    navController.navigate(Home) {
+                        popUpTo(Splash) {
+                            inclusive = true
+                        }
+                    }
+                })
+        }
+        composable<Home> {
+            HomeScreen(
+                navigateToMovieDetailScreen = { navController.navigate(Detail(it)) },
+                navigateToMovieSearchScreen = { navController.navigate(Search) })
+        }
+        composable<Search> {
+            SearchScreen(
+                navigateToPreviousScreen = { navController.navigateUp() },
+                navigateToMovieDetailScreen = { navController.navigate(Detail(it)) })
+        }
+        composable<Detail> {
+            DetailScreen(
+                navigateToPreviousScreen = { navController.navigateUp() })
+        }
     }
 }
