cbfcf99375cd5e2fe37ab7b66ec1ceda477a33d8
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-09-11 21:18:44 +0330

    Implement http response extension (#45)

--- app/src/main/java/com/example/movino/core/network/Client.kt
+++ app/src/main/java/com/example/movino/core/network/Client.kt
@@ -1,18 +1,14 @@
 package com.example.movino.core.network
 
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.ANDROID
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.statement.HttpResponse
-import io.ktor.client.statement.bodyAsText
-import io.ktor.http.URLProtocol
 import io.ktor.http.ContentType
+import io.ktor.http.URLProtocol
 import io.ktor.http.contentType
-import io.ktor.http.isSuccess
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -33,11 +29,4 @@ fun initialization() = HttpClient {
                 ignoreUnknownKeys = true
             })
     }
-}
-
-suspend inline fun <reified T> bodyOrThrow(response: HttpResponse): T {
-    if (!response.status.isSuccess()) {
-        throw Exception(response.bodyAsText())
-    }
-    return response.body<T>()
 }
